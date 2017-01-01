02a7c8221e985e2af3ccda500f2fe9a6e1b3712d
夏琳泰 <johndoe@example.com>
2017-01-01 20:50:08 +0800

    browserSync 测试的时候输出调试js

--- src/HtmlTemplate.php
+++ src/HtmlTemplate.php
@@ -14,6 +14,8 @@ namespace xltxlm\template;
  */
 trait HtmlTemplate
 {
+    private static $browserSync = false;
+
     /**
      * 在 runinvoke 里面,此方法跑到调用类的所有方法后面.
      */
@@ -22,5 +24,11 @@ trait HtmlTemplate
         $filepath = (new \ReflectionClass(static::class))->getFileName();
         $filepath = strtr($filepath, ['.php' => '.tpl.php']);
         eval("include '$filepath';");
+        if (!self::$browserSync && $_SERVER['SERVER_PORT'] == 3000) {
+            echo '<script id="__bs_script__">/*<![CDATA[*/ ' .
+                'document.write("<script async src=\'/browser-sync/browser-sync-client.js?v=2.18.5\'><\/script>"' .
+                '.replace("HOST", location.hostname)); //]]></script>';
+            self::$browserSync = true;
+        }
     }
 }
