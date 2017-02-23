e571ce8c8626ad612ffd933bcbcf6c82b5a46f0e
夏琳泰 <xltxlm@qq.com>
2017-02-23 16:27:58 +0800

    优化项目

--- src/HtmlTemplate.php
+++ src/HtmlTemplate.php
@@ -14,13 +14,32 @@ namespace xltxlm\template;
  */
 trait HtmlTemplate
 {
+    /** @var string 模板文件路径,如果没有指定,那么用类的同名模板文件 */
+    protected $HtmlTemplate = '';
+
+    /**
+     * @param string $HtmlTemplate
+     *
+     * @return static
+     */
+    public function setHtmlTemplate(string $HtmlTemplate)
+    {
+        $this->HtmlTemplate = $HtmlTemplate;
+
+        return $this;
+    }
+
     /**
      * 在 runinvoke 里面,此方法跑到调用类的所有方法后面.
      */
     final protected function getHtmlTemplate()
     {
-        $filepath = (new \ReflectionClass(static::class))->getFileName();
-        $filepath = strtr($filepath, ['.php' => '.tpl.php']);
-        eval("include '$filepath';");
+        if (!empty($this->HtmlTemplate)) {
+            eval("include '$this->HtmlTemplate';");
+        } else {
+            $filepath = (new \ReflectionClass(static::class))->getFileName();
+            $filepath = strtr($filepath, ['.php' => '.tpl.php']);
+            eval("include '$filepath';");
+        }
     }
 }
